786a51089c39c7f50d3f8575840ee3f9611cbc67
musichead99 <musichead99@naver.com>
2023-03-10 17:19:33 +0900

    initial commit

---
+++ src/main/java/net/chimhaha/clone/ChimhahaApplication.java
@@ -0,0 +1,13 @@
+package net.chimhaha.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimhahaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChimhahaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/chimhaha/clone/ChimhahaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chimhaha.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimhahaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
