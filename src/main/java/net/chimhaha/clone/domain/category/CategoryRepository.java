b25593ad9fef39044babf2e5c4c0f81665f8aeb8
musichead99 <musichead99@naver.com>
2023-03-22 16:28:10 +0900

    FEAT: 게시글을 카테고리별로 조회하기 위해 필요한 기능 작성
    CategoryRepository에 getReferenceByName메소드를 작성하여 카테고리 이름을 인자로 받았을 때 카테고리 객체를 조회할 수 있도록 함

--- src/main/java/net/chimhaha/clone/domain/category/CategoryRepository.java
+++ src/main/java/net/chimhaha/clone/domain/category/CategoryRepository.java
@@ -3,6 +3,10 @@ package net.chimhaha.clone.domain.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> getReferenceByName(String name);
 }
