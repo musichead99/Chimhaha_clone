c5b9e85df9d4f9b13a28d4a79125fa6f88bc2ebf
musichead99 <musichead99@naver.com>
2023-04-04 18:42:25 +0900

    FIX: DTO에 기본 생성자(@NoArgsConstructor) 추가
    클라이언트로부터 온 JSON요청을 DTO로 변환하는 과정에서 ObjectMapper가 DTO의 기본 생성자를 사용한다. 따라서 requestDto에서는 반드시 기본 생성자가 있어야 한다.

--- src/main/java/net/chimhaha/clone/web/dto/menu/MenuUpdateRequestDto.java
+++ src/main/java/net/chimhaha/clone/web/dto/menu/MenuUpdateRequestDto.java
@@ -1,8 +1,17 @@
 package net.chimhaha.clone.web.dto.menu;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
+@NoArgsConstructor
 public class MenuUpdateRequestDto {
     private String name;
+
+    @Builder
+    public MenuUpdateRequestDto(String name) {
+        this.name = name;
+    }
 }
