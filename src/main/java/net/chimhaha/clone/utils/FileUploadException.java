7da99f342b69b7724be7bef84c7823d98e2b02fc
musichead99 <musichead99@naver.com>
2023-05-22 17:02:34 +0900

    FEAT: 이미지 파일 업로드 시 핸들링할 예외 작성
    FileUploadException: 파일 업로드 시 발생하는 예외
    FileDownloadException: 파일을 불러올(다운로드) 때 발생하는 예외

---
+++ src/main/java/net/chimhaha/clone/utils/FileUploadException.java
@@ -0,0 +1,11 @@
+package net.chimhaha.clone.utils;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
