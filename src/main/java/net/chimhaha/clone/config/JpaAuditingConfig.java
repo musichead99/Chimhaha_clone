efc248485e309c9f057650a7d795b14c1b307f1f
musichead99 <musichead99@naver.com>
2023-05-31 15:03:26 +0900

    REFACTOR: config패키지 생성. JpaAuditingConfig 이동

--- src/main/java/net/chimhaha/clone/config/JpaAuditingConfig.java
+++ src/main/java/net/chimhaha/clone/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package net.chimhaha.clone;
+package net.chimhaha.clone.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
