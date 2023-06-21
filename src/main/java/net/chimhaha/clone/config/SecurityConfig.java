96e5c836f1cd490144b2615c8397d18ba8a9fa8b
musichead99 <musichead99@naver.com>
2023-06-21 15:41:09 +0900

    FEAT: 네이버 OAuth2 로그인 적용
    build.gradle:
     - 스프링 시큐리티, 스프링 oauth2-client 의존성 추가
    
    application.properties:
     - 미리 정의되어 있지 않은 provider의 OAuth2 로그인을 구현하는 것이기 때문에 추가적인 정보를 입력해 두어야 한다.
     - client_id, client_secret, client_name, authorization_grant_type, redirect_uri 등
    
    SecurityConfig: 스프링 시큐리티를 사용하기 위한 설정
     - oauth2 로그인을 할 것이기 때문에 formLogin은 사용하지 않음
     - 아직은 접근 권한에 대해 세부적으로 적용하지 않고 oauth2 로그인과 h2-console에 접근하는 것만 빼고는 모두 권한을 체크하도록 설정
     - oauth2로그인이 완료되고 provider로부터 받은 유저 정보를 처리하는 클래스 CustomOAuth2UserService를 userService로 넘겨준다
    
    CustomOAuth2User: CustomOAuth2UserService의 loadUser()메소드가 반환할 클래스
     - OAuth2 로그인에 사용할 때는 OAuth2User, 일반로그인 시는 UserDetail을 구현해야 하지만, 일반 로그인 방식은 사용하지 않기 때문에 OAuth2User만 구현
    
    OAuth2UserInfo: provider에 종속되지 않게 로그인에 필요한 정보들을 가공하여 전달하기 위한 일종의 dto역할
     - Member 엔티티에 저장할 정보들과 attributes를 필드로 가진다.
     - 정적 팩토리 메소드 of() 로 객체를 생성하며 provider에 따라 다른 파싱 방법을 사용해 객체를 생성한다. (ofNaver, ofGoogle, ...)
    
    CustomOAuth2UserService: provider로부터 전달받은 정보들로 회원가입 또는, 로그인 처리를 한다.
     - loadUser() 메소드는 전달받은 userRequest로부터 사용자 정보를 분리하여 회원가입 처리를 하고 CustomOAuth2User를 반환한다.

---
+++ src/main/java/net/chimhaha/clone/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package net.chimhaha.clone.config;
+
+import lombok.RequiredArgsConstructor;
+import net.chimhaha.clone.config.auth.CustomOAuth2UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService oauth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .headers().frameOptions().sameOrigin()
+
+                .and()
+                .csrf().disable()
+                .cors().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+
+                .authorizeRequests()
+                .antMatchers("/h2-console/**", "/oauth2/authorization/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oauth2UserService)
+
+                .and()
+                .and()
+                .build();
+    }
+}
---
+++ src/main/java/net/chimhaha/clone/config/auth/CustomOAuth2User.java
@@ -0,0 +1,35 @@
+package net.chimhaha.clone.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import net.chimhaha.clone.domain.member.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+    private final Map<String, Object> attributes;
+    private final Member member;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> member.getRole().getTitle());
+        return authorities;
+    }
+
+    @Override
+    public String getName() {
+        return member.getName();
+    }
+}
